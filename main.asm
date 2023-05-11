d585bcf18305e5fad2ec1fa51149260c44760e32
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2023-05-11 12:50:57 +0530

    functions

--- main.asm
+++ main.asm
@@ -3,25 +3,54 @@ format ELF64 executable 3
 segment readable executable
 
 entry $
-        ; read from stdin
-        xor     rax, rax        ; sys_read
-        xor     rdi, rdi        ; stdin
-        mov     rsi, buf        ; buffer address
-        mov     rdx, 80
-        syscall
+        mov     rax, buf
+        mov     rdi, 80
+        call    read_console
 
-        ; write to stdout
-        mov     rax, 1          ; sys_write
-        mov     rdi, 1          ; stdout
-        ; rsi should still be address of buffer
-        mov     rdx, 80
-        syscall
+        mov     rdi, rax        ; num bytes to write
+        mov     rax, buf        ; buf addr
+        call    write_console
 
         ; exit the program
         xor     rdi, rdi        ; exit code
         mov     rax, 60         ; sys_exit
         syscall
 
+; write_console
+; writes outputs from buffer to stdout
+; inputs:
+;     rax : buf addr
+;     rdi : buf size
+; outpus:
+;     rax : number of bytes written (-1 if error)
+write_console:
+        mov     rsi, rax        ; buffer addr
+        mov     rdx, rdi        ; buffer size
+
+        ; read from stdin
+        mov     rax, 1          ; buffer address
+        mov     rdi, 1          ; buffer size
+        syscall
+        ret
+
+
+; read_console
+; reads input from stdin into a buffer
+; inputs:
+;     rax : buf addr
+;     rdi : buf size
+; outpus:
+;     rax : number of bytes read (-1 if error)
+read_console:
+        mov     rsi, rax        ; buffer addr
+        mov     rdx, rdi        ; buffer size
+
+        ; read from stdin
+        xor     rax, rax        ; buffer address
+        xor     rdi, rdi        ; buffer size
+        syscall
+        ret
+
 segment readable writeable
 
-buf     rb 80
+buf     rb      80
